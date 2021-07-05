dec3618ce51986ba63fb27e8f43700ebd3437363
ClaudeSeo <ehdaudtj@gmail.com>
2021-07-05 21:54:18 +0900

    feat: add express middleware (helmet, compression, cors)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.use(helmet());
+  app.use(compression());
+  app.enableCors();
   await app.listen(configService.get<number>('port') || 3000);
 }
 
